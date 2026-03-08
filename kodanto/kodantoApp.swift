af38aa0b8c7fb6fe1adc54244f408f9b1e884ee4
Matthew Allison <66563291+Sir-Goose@users.noreply.github.com>
2026-03-08 12:44:57 +0000

    Initial Commit

---
+++ kodanto/kodantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  kodantoApp.swift
+//  kodanto
+//
+//  Created by Matthew Allison on 2026/03/08.
+//
+
+import SwiftUI
+
+@main
+struct kodantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
